9a35c1f4408901f63d3c05c25a435592c092e4ae
kratoar <mael.fallet@corellis.eu>
2020-09-07 00:18:44 +0200

    Fixed menu beginning

---
+++ src/components/fixed-menu.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { get_img_path } from "../functions/get_images";
+import Menu from "./menu";
+import { useStaticQuery, graphql } from "gatsby";
+import { process_menu_datas } from "../functions/process_menu_datas";
+
+class FixedMenu extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            menus: process_menu_datas(props.datas, 'fixed-menu'),
+            visible: false
+        }
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = ev => {
+        this.setState({
+            visible: window.scrollY > 200
+        });
+    };
+
+    render () {
+        return (
+            <div id="fixed-menu" className="transition" style={{top: this.state.visible ? 0 : -50, boxShadow: this.state.visible ? null : 'unset'}}>
+                <div className="fixed-menu-container">
+                    <div className="fixed-menu-logo">
+                        <img src={get_img_path('/icons/header-logo.png')} alt="header-logo"/>
+                    </div>
+                    <div className="fixed-menus">
+                        {this.state.menus.map((menu, key) => {
+                            return (
+                                <Menu key={key} prop_key={key} menu={menu} />
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        );
+    };
+}
+
+FixedMenu.propTypes = {
+
+};
+
+FixedMenu.defaultProps = {
+
+}
+
+export default FixedMenu;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -9,17 +9,40 @@ import ClinicalStudies from "../components/home/clinical-studies"
 import FollowInstagram from "../components/home/follow-instagram"
 import GetStarted from "../components/home/get-started"
 import Slides from "../components/home/slides"
+import FixedMenu from "../components/fixed-menu"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home"/>
-    {/* <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.css"></link> */}
-    {/* <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.js"></script> */}
-    <Slides from='home'/>
-    <ClinicalStudies/>
-    <FollowInstagram/>
-    <GetStarted/>
-  </Layout>
-)
+const IndexPage = () => {
+  const datas = useStaticQuery(graphql`
+    {
+      allMysqlHeaderBottom {
+        edges {
+          node {
+            container
+            name
+            position
+            type
+            under
+            url
+            variant
+            mysqlId
+          }
+        }
+      }
+    }
+  `).allMysqlHeaderBottom.edges;
+
+  return (
+    <Layout>
+      <FixedMenu datas={datas}/>
+      <SEO title="Home"/>
+      {/* <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.css"></link> */}
+      {/* <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.js"></script> */}
+      <Slides from='home'/>
+      <ClinicalStudies/>
+      <FollowInstagram/>
+      <GetStarted/>
+    </Layout>
+  );
+}
 
 export default IndexPage
