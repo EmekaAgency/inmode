c712f658186cc07b769ed2cff94e52690b3dd0a8
kratoar <mael.fallet@gmail.fr>
2021-05-06 02:59:11 +0200

    ClinicFinder search

--- src/components/ClinicFinder/index.tsx
+++ src/components/ClinicFinder/index.tsx
@@ -4,6 +4,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 import { InmodePanel_Clinic_Interface } from '../interfaces';
 
 import './index.css';
+import { allByClass, oneById } from '../../functions/selectors';
 
 const ClinicalFinder = ({}:ClinicalFinder_Interface) => {
 
@@ -32,19 +33,59 @@ const ClinicalFinder = ({}:ClinicalFinder_Interface) => {
         }
     `).allStrapiClinicFinders.nodes));
 
+    const [search, setSearch]:[any, React.Dispatch<any>] = React.useState(undefined);
+    // const [rest, setRest]:[number, React.Dispatch<number>] = React.useState(0);
+
     const switchTreatmentsOpened = (e:any) => {
         e.currentTarget.parentElement.parentElement.classList.contains('treatments') ?
         e.currentTarget.parentElement.parentElement.classList.remove('treatments') :
         e.currentTarget.parentElement.parentElement.classList.add('treatments');
     }
 
+    const updateSearch = (e:React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setSearch(e.currentTarget.value.toLowerCase());
+    }
+
+    const displayable = (clinic:InmodePanel_Clinic_Interface):boolean => {
+        let _retour = false;
+        if(!clinic) {
+            return false;
+        }
+        if(search == undefined) {
+            return true;
+        }
+        if(_retour == false && clinic.street) {
+            _retour = clinic.street.toLowerCase().includes(search) ? true : false;
+        }
+        if(_retour == false && clinic.doctor) {
+            _retour = clinic.doctor.toLowerCase().includes(search) ? true : false;
+        }
+        if(_retour == false && clinic.city) {
+            _retour = clinic.city.toLowerCase().includes(search) ? true : false;
+        }
+        if(_retour == false && clinic.treatments && clinic.treatments.length > 0) {
+            _retour = clinic.treatments.map(treatment => treatment.MenuParams.title).join('').toLowerCase().includes(search) ? true : false;
+        }
+        return _retour;
+    }
+
+    React.useEffect(() => {
+        oneById("search-clinic-indicator").innerText = `${typeof document != "undefined" ? document.querySelectorAll('.clinic-item').length : 0}/${clinics ? clinics.length : 0}`;
+    }, [search]);
+
     return (
         <div className="clinic-finder">
             <div className="container">
                 <h2 className="title">Inmode Clinic Finder</h2>
                 <h3 className="subtitle">Authorised practioner list</h3>
+                {/* <div id="search-clinic-indicator"><span>{rest}</span>/{clinics ? clinics.length : 0}</div> */}
+                <div id="search-clinic-indicator"></div>
+                {/* <div id="search-clinic-indicator">{allByClass('clinic-item') ? allByClass('clinic-item').length : 0}/{clinics ? clinics.length : 0}</div> */}
+                <input id="clinic-finder-search" type="search" placeholder="Search..." onChange={(e) => updateSearch(e)}/>
+                <div className="bottom-border"></div>
                 {clinics && clinics.map((clinic:InmodePanel_Clinic_Interface) => {
-                    if(clinic) {
+                    if(clinic && displayable(clinic)) {
                         Object.keys(clinic).forEach((elem) => elem ? elem.length < 2 ? undefined : elem : undefined);
                         return (
                             <div className="clinic-item">
